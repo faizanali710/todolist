fe6c4223e4d7378afa776cee1e0ea27b09a33f7a
faizanali710 <faiziali1070@gmail.com>
2022-07-04 00:39:15 +0500

    todolist

---
+++ code.js
@@ -0,0 +1,15 @@
+const form = document.forms["myForm"];
+const input = form["task"];
+const ul= document.getElementById("taskList");
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const val = input.value;
+
+    const newLi = document.createElement("li");
+    newLi.innerHTML = val + "<i onclick=\"deleteTask(this)\" class=\"fa-solid fa-xmark\" style=\"float: right;\"></i>";
+    ul.appendChild(newLi);
+})
+
+function deleteTask(x) {
+    x.parentElement.style.textDecoration="line-through";
+}
